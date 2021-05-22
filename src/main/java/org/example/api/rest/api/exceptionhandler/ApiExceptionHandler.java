be545be797ecae6df5708347e403c87b8c9b93c4
Victor Dias <vgdias@gmail.com>
2021-05-22 13:24:46 -0300

    Define ApiExceptionHandler estendendo ResponseEntityExceptionHandler e sobrescreve handleExceptionInternal para customizar o body da resposta Http com a mensagem das excecoes lancadas pelo Spring

--- src/main/java/org/example/api/rest/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/org/example/api/rest/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,42 +1,66 @@
 package org.example.api.rest.api.exceptionhandler;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 import org.example.api.rest.domain.exception.DependenciaNaoEncontradaException;
 import org.example.api.rest.domain.exception.EntidadeEmUsoException;
 import org.example.api.rest.domain.exception.EntidadeNaoEncontradaException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
-		ExceptionMessage message = ExceptionMessage.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem(e.getMessage()).build();
+	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex, 
+			WebRequest request) {
 
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
+		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), 
+				HttpStatus.NOT_FOUND, request);
 	}
 
 	@ExceptionHandler(EntidadeEmUsoException.class)
-	public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e) {
-		ExceptionMessage message = ExceptionMessage.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem(e.getMessage()).build();
+	public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException ex,
+			WebRequest request) {
 
-		return ResponseEntity.status(HttpStatus.CONFLICT).body(message);
+		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), 
+				HttpStatus.CONFLICT, request);
 	}
 
 	@ExceptionHandler(DependenciaNaoEncontradaException.class)
-	public ResponseEntity<?> tratarDependenciaNaoEncontradaException(DependenciaNaoEncontradaException e) {
-		ExceptionMessage message = ExceptionMessage.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem(e.getMessage()).build();
+	public ResponseEntity<?> tratarDependenciaNaoEncontradaException(DependenciaNaoEncontradaException ex,
+			WebRequest request) {
 
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
+		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), 
+				HttpStatus.BAD_REQUEST, request);
+	}
+
+	/**
+	 * Customiza a mensagem da excecao retornada no body de todas as excecoes.
+	 * As excecoes internas do Spring retornam null no body, por isso recebem a mensagem
+	 * com a causa da excecao. As excecoes da aplicacao retornam uma String com a mensagem
+	 * de erro, que eh inserida no body.
+	 */
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+
+		if (Objects.isNull(body)) {
+			body = ExceptionMessage.builder()
+					.dataHora(LocalDateTime.now())
+					.mensagem(status.getReasonPhrase()).build();
+		} else if (body instanceof String) {
+			body = ExceptionMessage.builder()
+					.dataHora(LocalDateTime.now())
+					.mensagem((String) body).build();
+		}
+
+		return super.handleExceptionInternal(ex, body, headers, status, request);
 	}
 }
