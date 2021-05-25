3791ef5e8ce2f4c697ccb056352e4fdb1efdf02c
Victor Dias <vgdias@gmail.com>
2021-05-25 12:30:28 -0300

    Criado handleTypeMismatch em ApiExceptionHanler, que trata MethodArgumentTypeMismatchException - lancada quando a requisicao possui uma @PathVariable invalida

--- src/main/java/org/example/api/rest/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/org/example/api/rest/api/exceptionhandler/ApiExceptionHandler.java
@@ -7,6 +7,7 @@ import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.example.api.rest.domain.exception.DependenciaNaoEncontradaException;
 import org.example.api.rest.domain.exception.EntidadeEmUsoException;
 import org.example.api.rest.domain.exception.EntidadeNaoEncontradaException;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
@@ -67,6 +69,34 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 				status, request);
 	}
 
+	@Override
+	protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers,
+	        HttpStatus status, WebRequest request) {
+	    
+	    if (ex instanceof MethodArgumentTypeMismatchException) {
+	        return handleMethodArgumentTypeMismatch(
+	                (MethodArgumentTypeMismatchException) ex, headers, status, request);
+	    }
+
+	    return super.handleTypeMismatch(ex, headers, status, request);
+	}
+	
+	private ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		String detail = String.format("O parametro de URL '%s' recebeu o valor invalido '%s'", 
+				ex.getName(), ex.getValue());
+
+		ExceptionMessage exceptionMessage = ExceptionMessage.builder()
+				.status(status.value())
+				.title("Mensagem invalida")
+				.detail(detail)
+				.build();
+
+		return handleExceptionInternal(ex, exceptionMessage, new HttpHeaders(), 
+				status, request);
+	}
+	
 	/**
 	 * Customiza as excecoes genericas geradas por erro de sintaxe no corpo da requisicao
 	 */
@@ -141,7 +171,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		String path = ex.getPath().stream()
 				.map(ref -> ref.getFieldName())
 				.collect(Collectors.joining("."));
-		String detail = String.format("A propriedade '%s' recebeu o valor invalido '%s'. Informe um valor compativel com o tipo '%s'", 
+		String detail = String.format("A propriedade '%s' recebeu o valor invalido '%s'", 
 				path , ex.getValue(), ex.getTargetType().getSimpleName());
 
 		ExceptionMessage exceptionMessage = ExceptionMessage.builder()
